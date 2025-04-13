7a293e0f1b029063f3663ffa584711027ef2ff69
Matvej Kurakin <matvejkurakin7755@gmail.com>
2025-04-13 15:06:44 +0300

    add example

--- Enemy_firepower/main.py
+++ Enemy_firepower/main.py
@@ -6,7 +6,7 @@ import os
 from gen_func import *
 import time
 
-def generate_matrix_and_vector():
+def generate_matrix_and_vector(new_c=None):
 
     N = int(entry_n.get())
     c_min = int(entry_c_min.get())
@@ -29,6 +29,8 @@ def generate_matrix_and_vector():
         raise ValueError("Минимальное значение должно быть меньше максимального.")
 
     c = create_c(N, c_min, c_max)
+    if(new_c is not None):
+        c = new_c
 
     if(is_sort_matrix_y == "1"):
         c = increase_by_row(c)
@@ -43,7 +45,7 @@ def generate_matrix_and_vector():
     show_c(c)
 
     cur_time = time.time()
-    total_optimal_max, _, perm_1, perm_2 = optimal(c, k)
+    total_optimal_max, optimal_max, perm_1, perm_2 = optimal(c, k)
     time_optimal = time.time() - cur_time
     res_optimal_max = np.zeros(c.shape)
     for i in range(len(perm_1)):
@@ -51,7 +53,7 @@ def generate_matrix_and_vector():
         res_optimal_max[perm_2[i]][i] = 2
 
     cur_time = time.time()
-    total_naive_max, _, perm_1, perm_2 = naive(c, k)
+    total_naive_max, naive_max, perm_1, perm_2 = naive(c, k)
     time_naive = time.time() - cur_time
     res_naive_max = np.zeros(c.shape)
     for i in range(len(perm_1)):
@@ -59,21 +61,36 @@ def generate_matrix_and_vector():
         res_naive_max[perm_2[i]][i] = 2
 
     sum_entry_veng_max.delete(0, tk.END)
-    sum_entry_veng_max.insert(0, str(f"{round(total_optimal_max, 2)} ({round(time_optimal, 2)} сек.)"))
+    sum_entry_veng_max.insert(0, str(f"{round(optimal_max, 2)}"))
+
+    sum_entry_veng_max_res.delete(0, tk.END)
+    sum_entry_veng_max_res.insert(0, str(f"{round(total_optimal_max, 2)}"))
+
+    sum_entry_veng_max_time.delete(0, tk.END)
+    sum_entry_veng_max_time.insert(0, str(f"{round(time_optimal, 2)} сек."))
 
     sum_entry_greedy_d.delete(0, tk.END)
-    sum_entry_greedy_d.insert(0, str(f"{round(total_naive_max, 2)} ({round(time_naive, 2)} сек.)"))
+    sum_entry_greedy_d.insert(0, str(f"{round(naive_max, 2)}"))
+
+    sum_entry_greedy_d_res.delete(0, tk.END)
+    sum_entry_greedy_d_res.insert(0, str(f"{round(total_naive_max, 2)}"))
+
+    sum_entry_greedy_d_time.delete(0, tk.END)
+    sum_entry_greedy_d_time.insert(0, str(f"{round(time_naive, 2)} сек."))
 
     error.delete(0, tk.END)
-    error.insert(0, str(f"{round((total_naive_max - total_optimal_max), 2)} ({round(((total_naive_max - total_optimal_max) / total_optimal_max) * 100, 2)}%)"))
+    error.insert(0, str(f"{round((optimal_max - naive_max), 2)}"))
+
+    error_res.delete(0, tk.END)
+    error_res.insert(0, str(f"{round((total_naive_max - total_optimal_max), 2)}"))
 
     show_matrix_on_frame(c, res_optimal_max, frame_veng_max)
     show_matrix_on_frame(c, res_naive_max, frame_greedy_d)
 
-    tk.Label(left_frame, bg=select_color_1, image=img_c_min).grid(row=24, column = 0)
-    tk.Label(left_frame, text="Первое арт. подразделение", bg=background_color, font=font_style).grid(row=24, column = 1, columnspan=3, sticky="w")
-    tk.Label(left_frame, bg=select_color_2, image=img_c_min).grid(row=25, column=0)
-    tk.Label(left_frame, text="Второе арт. подразделение", bg=background_color, font=font_style).grid(row=25, column=1, columnspan=3, sticky="w")
+    tk.Label(left_frame, bg=select_color_1, image=img_c_min).grid(row=26, column = 0)
+    tk.Label(left_frame, text="Первое арт. подразделение", bg=background_color, font=font_style).grid(row=26, column = 1, columnspan=3, sticky="w")
+    tk.Label(left_frame, bg=select_color_2, image=img_c_min).grid(row=27, column=0)
+    tk.Label(left_frame, text="Второе арт. подразделение", bg=background_color, font=font_style).grid(row=27, column=1, columnspan=3, sticky="w")
 
 def show_c(matrix):
     for widget in frame_c_and_x.winfo_children():
@@ -136,6 +153,25 @@ def show_matrix_on_frame(matrix, res, frame):
                              borderwidth=1,
                              relief="solid")
             label.grid(row=i + 2, column=j + 1)
+
+def example_matrix_3():
+    entry_n.delete(0, tk.END)
+    entry_n.insert(0, "3")
+
+    entry_c_min.delete(0, tk.END)
+    entry_c_min.insert(0, "2")
+
+    entry_c_max.delete(0, tk.END)
+    entry_c_max.insert(0, "5")
+
+    entry_k.delete(0, tk.END)
+    entry_k.insert(0, "5.0")
+
+    c = np.array([[5, 4, 2],
+                  [4, 5, 4],
+                  [2, 4, 5]])
+    generate_matrix_and_vector(c)
+
 def on_close():
     root.destroy()
     sys.exit()
@@ -218,7 +254,7 @@ right_frame.grid(row=0, column=1, padx=10, sticky='nsew', pady=(10,0))
 
 #####################################
 
-tk.Label(left_frame, text="Входные параметры", bg=background_color, font=font_style_head).grid(row=0, columnspan=4)
+tk.Label(left_frame, text="Входные параметры", bg=background_color, font=font_style_head).grid(row=0, columnspan=5)
 
 original_image = Image.open(path_img_group)
 resized_image = original_image.resize((img_size, img_size), Image.LANCZOS)
@@ -226,12 +262,12 @@ img_group = ImageTk.PhotoImage(resized_image)
 label = tk.Label(left_frame, bg=background_color, image = img_group) # put the image on a label
 label.grid(row = 1, column = 0) # put the label in the grid
 
-tk.Label(left_frame, text="Число подразделений:", bg=background_color, font=font_style).grid(row=1, column=1)
+tk.Label(left_frame, text="Число подразделений:", bg=background_color, font=font_style).grid(row=1, column=1, columnspan=2)
 entry_n = tk.Entry(left_frame, bg = entry_color, font=font_style, width=entry_width)
 entry_n.insert(0, "15")
-entry_n.grid(row=1, column=2)
+entry_n.grid(row=1, column=3)
 
-tk.Label(left_frame, text="не более 15", bg=background_color, font=font_style).grid(row=1, column=3, sticky="w")
+tk.Label(left_frame, text="не более 15", bg=background_color, font=font_style).grid(row=1, column=4, sticky="w")
 
 original_image = Image.open(path_img_c_min)
 resized_image = original_image.resize((img_size, img_size), Image.LANCZOS)
@@ -240,12 +276,12 @@ label = tk.Label(left_frame, bg=background_color, image = img_c_min)
 label.image = img_c_min
 label.grid(row = 2, column = 0)
 
-tk.Label(left_frame, text="Минимальная мощь:", bg=background_color, font=font_style).grid(row=2, column=1)
+tk.Label(left_frame, text="Минимальная мощь:", bg=background_color, font=font_style).grid(row=2, column=1, columnspan=2)
 entry_c_min = tk.Entry(left_frame, bg = entry_color, font=font_style, width=entry_width)
 entry_c_min.insert(0, "10")
-entry_c_min.grid(row=2, column=2)
+entry_c_min.grid(row=2, column=3)
 
-tk.Label(left_frame, text="не менее 0", bg=background_color, font=font_style).grid(row=2, column=3, sticky="w")
+tk.Label(left_frame, text="не менее 0", bg=background_color, font=font_style).grid(row=2, column=4, sticky="w")
 
 original_image = Image.open(path_img_c_max)
 resized_image = original_image.resize((img_size, img_size), Image.LANCZOS)
@@ -254,12 +290,12 @@ label = tk.Label(left_frame, bg=background_color, image = img_c_max)
 label.image = img_c_max
 label.grid(row = 3, column = 0)
 
-tk.Label(left_frame, text="Максимальная мощь:", bg=background_color, font=font_style).grid(row=3, column=1)
+tk.Label(left_frame, text="Максимальная мощь:", bg=background_color, font=font_style).grid(row=3, column=1, columnspan=2)
 entry_c_max = tk.Entry(left_frame, bg = entry_color, font=font_style, width=entry_width)
 entry_c_max.insert(0, "99")
-entry_c_max.grid(row=3, column=2)
+entry_c_max.grid(row=3, column=3)
 
-tk.Label(left_frame, text="не более 99999", bg=background_color, font=font_style).grid(row=3, column=3, sticky="w")
+tk.Label(left_frame, text="не более 99999", bg=background_color, font=font_style).grid(row=3, column=4, sticky="w")
 
 original_image = Image.open(path_img_k)
 resized_image = original_image.resize((img_size, img_size), Image.LANCZOS)
@@ -268,76 +304,97 @@ label = tk.Label(left_frame, bg=background_color, image = img_k)
 label.image = img_k
 label.grid(row = 4, column = 0)
 
-tk.Label(left_frame, text="Коеф. снижения мощи:", bg=background_color, font=font_style).grid(row=4, column=1)
+tk.Label(left_frame, text="Коеф. снижения мощи:", bg=background_color, font=font_style).grid(row=4, column=1, columnspan=2)
 entry_k = tk.Entry(left_frame, bg = entry_color, font=font_style, width=entry_width)
-entry_k.insert(0, "1.50")
-entry_k.grid(row=4, column=2)
+entry_k.insert(0, "5.00")
+entry_k.grid(row=4, column=3)
 
-tk.Label(left_frame, text="не менее 1.00", bg=background_color, font=font_style).grid(row=4, column=3, sticky="w")
+tk.Label(left_frame, text="не менее 1.00", bg=background_color, font=font_style).grid(row=4, column=4, sticky="w")
 
 #####################################
 
-tk.Label(left_frame, text="Расстановка по строкам", bg=background_color, font=font_style_head).grid(row=6, columnspan=4)
+tk.Label(left_frame, text="Расстановка по строкам", bg=background_color, font=font_style_head).grid(row=6, columnspan=5)
 
 entry_is_sort_matrix_y = tk.StringVar(value=0)
 
 r_sort_y_1 = tk.Radiobutton(left_frame, text="Без расстановки", bg=background_color, variable=entry_is_sort_matrix_y, value=0, font=font_style)
-r_sort_y_1.grid(row=7, columnspan=4, sticky="w")
+r_sort_y_1.grid(row=7, columnspan=5, sticky="w")
 
 r_sort_y_2 = tk.Radiobutton(left_frame, text="Возрастание по строкам", bg=background_color, variable=entry_is_sort_matrix_y, value=1, font=font_style)
-r_sort_y_2.grid(row=8, columnspan=4, sticky="w")
+r_sort_y_2.grid(row=8, columnspan=5, sticky="w")
 
 r_sort_y_3 = tk.Radiobutton(left_frame, text="Убывание по строкам", bg=background_color, variable=entry_is_sort_matrix_y, value=2, font=font_style)
-r_sort_y_3.grid(row=9, columnspan=4, sticky="w")
+r_sort_y_3.grid(row=9, columnspan=5, sticky="w")
 
 #####################################
 
-tk.Label(left_frame, text="Расстановка по столбцам", bg=background_color, font=font_style_head).grid(row=10, columnspan=4)
+tk.Label(left_frame, text="Расстановка по столбцам", bg=background_color, font=font_style_head).grid(row=10, columnspan=5)
 
 entry_is_sort_matrix_x = tk.StringVar(value=0)
 
 r_sort_x_1 = tk.Radiobutton(left_frame, text="Без расстановки", bg=background_color, variable=entry_is_sort_matrix_x, value=0, font=font_style)
-r_sort_x_1.grid(row=11, columnspan=4, sticky="w")
+r_sort_x_1.grid(row=11, columnspan=5, sticky="w")
 
 r_sort_x_2 = tk.Radiobutton(left_frame, text="Возрастание по столбцам", bg=background_color, variable=entry_is_sort_matrix_x, value=1, font=font_style)
-r_sort_x_2.grid(row=12, columnspan=4, sticky="w")
+r_sort_x_2.grid(row=12, columnspan=5, sticky="w")
 
 r_sort_x_3 = tk.Radiobutton(left_frame, text="Убывание по столбцам", bg=background_color, variable=entry_is_sort_matrix_x, value=2, font=font_style)
-r_sort_x_3.grid(row=13, columnspan=4, sticky="w")
+r_sort_x_3.grid(row=13, columnspan=5, sticky="w")
 
 #####################################
 
-tk.Label(left_frame, text="Результат", bg=background_color, font=font_style_head).grid(row=18, columnspan=4)
+tk.Label(left_frame, text="Проверка работы", bg=background_color, font=font_style_head).grid(row=18, columnspan=5)
 
-original_image = Image.open(path_img_empty)
+button_generate = tk.Button(left_frame, text="Матрица 3x3", font=font_style, bg=button_color, command=example_matrix_3)
+button_generate.grid(row=19, columnspan=5, pady=10)
+
+#####################################
+
+tk.Label(left_frame, text="Результат", bg=background_color, font=font_style_head).grid(row=20, columnspan=5)
+
+'''original_image = Image.open(path_img_empty)
 resized_image = original_image.resize((img_size, img_size), Image.LANCZOS)
 my_image = ImageTk.PhotoImage(resized_image)
 label = tk.Label(left_frame, bg=background_color, image = my_image)
 label.image = my_image
-label.grid(row = 19, column = 0)
+label.grid(row = 19, column = 0)'''
 
-tk.Label(left_frame, text="Оптимальный", bg=background_color, font=font_style).grid(row=19, column=1)
-sum_entry_veng_max = tk.Entry(left_frame, bg = entry_color, font=font_style, width=entry_width * 2)
-sum_entry_veng_max.grid(row=19, column=2, columnspan=2)
+tk.Label(left_frame, text="S_max", bg=background_color, font=font_style).grid(row=21, column=2)
+tk.Label(left_frame, text="S", bg=background_color, font=font_style).grid(row=21, column=3)
+tk.Label(left_frame, text="Время", bg=background_color, font=font_style).grid(row=21, column=4)
 
-label = tk.Label(left_frame, bg=background_color, image = my_image)
+tk.Label(left_frame, text="Оптимальный", bg=background_color, font=font_style).grid(row=22, column=0, columnspan=2, sticky="w")
+sum_entry_veng_max = tk.Entry(left_frame, bg = entry_color, font=font_style, width=int(entry_width*1.2))
+sum_entry_veng_max.grid(row=22, column=2)
+sum_entry_veng_max_res = tk.Entry(left_frame, bg = entry_color, font=font_style, width=int(entry_width*1.2))
+sum_entry_veng_max_res.grid(row=22, column=3)
+sum_entry_veng_max_time = tk.Entry(left_frame, bg = entry_color, font=font_style, width=int(entry_width*1.2))
+sum_entry_veng_max_time.grid(row=22, column=4)
+
+'''label = tk.Label(left_frame, bg=background_color, image = my_image)
 label.image = my_image
-label.grid(row = 21, column = 0)
+label.grid(row = 21, column = 0)'''
 
-tk.Label(left_frame, text="Наивный", bg=background_color, font=font_style).grid(row=21, column=1)
-sum_entry_greedy_d = tk.Entry(left_frame, bg = entry_color, font=font_style, width=entry_width * 2)
-sum_entry_greedy_d.grid(row=21, column=2, columnspan=2)
+tk.Label(left_frame, text="Наивный", bg=background_color, font=font_style).grid(row=23, column=0, columnspan=2, sticky="w")
+sum_entry_greedy_d = tk.Entry(left_frame, bg = entry_color, font=font_style, width=int(entry_width*1.2))
+sum_entry_greedy_d.grid(row=23, column=2)
+sum_entry_greedy_d_res = tk.Entry(left_frame, bg = entry_color, font=font_style, width=int(entry_width*1.2))
+sum_entry_greedy_d_res.grid(row=23, column=3)
+sum_entry_greedy_d_time = tk.Entry(left_frame, bg = entry_color, font=font_style, width=int(entry_width*1.2))
+sum_entry_greedy_d_time.grid(row=23, column=4)
 
-label = tk.Label(left_frame, bg=background_color, image = my_image)
+'''label = tk.Label(left_frame, bg=background_color, image = my_image)
 label.image = my_image
-label.grid(row = 22, column = 0)
+label.grid(row = 22, column = 0)'''
 
-tk.Label(left_frame, text="Погрешность", bg=background_color, font=font_style).grid(row=22, column=1)
-error = tk.Entry(left_frame, bg = entry_color, font=font_style, width=entry_width * 2)
-error.grid(row=22, column=2, columnspan=2)
+tk.Label(left_frame, text="Погрешность", bg=background_color, font=font_style).grid(row=24, column=0, columnspan=2, sticky="w")
+error = tk.Entry(left_frame, bg = entry_color, font=font_style, width=int(entry_width*1.2))
+error.grid(row=24, column=2)
+error_res = tk.Entry(left_frame, bg = entry_color, font=font_style, width=int(entry_width*1.2))
+error_res.grid(row=24, column=3)
 
 button_generate = tk.Button(left_frame, text="Сгенерировать", font=font_style, bg=button_color, command=generate_matrix_and_vector)
-button_generate.grid(row=23, columnspan=4, pady=10)
+button_generate.grid(row=25, columnspan=5, pady=10)
 
 #####################################
 
